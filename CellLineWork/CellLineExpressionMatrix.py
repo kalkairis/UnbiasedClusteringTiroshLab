42e4cc29e66282eea2ab3b973cd2ab07c9011413
iriskalka <kalka.iris@gmail.com>
2018-03-28 17:55:44 +0300

    config file

--- CellLineWork/CellLineExpressionMatrix.py
+++ CellLineWork/CellLineExpressionMatrix.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from PreprocessingPipeline.Transformers.TransformToTPMAndCenter import TransformToTPMAndCenter
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
