5e06d5e067960dc3a9a30d0ca87ef5335ac62b3b
iriskalka <kalka.iris@gmail.com>
2018-04-25 13:58:28 +0300

    changes to irrelevant file

--- PreprocessingPipeline/Transformers/ClusterExpressionIntoTree.py
+++ PreprocessingPipeline/Transformers/ClusterExpressionIntoTree.py
@@ -1,22 +1,22 @@
 from PreprocessingPipeline.Transformers.Transformer import Transformer
 from scipy.cluster.hierarchy import linkage, dendrogram
 import matplotlib.pyplot as plt
+import config
 from config import BasePaths
 from Utilities import *
 
 
 class ClusterExpressionIntoTree(Transformer):
     def transform_aux(self, expression_object, *args, **kwargs):
-        tree = linkage(expression_object.expression_matrix)
-        fig = plt.figure()
+        tree = linkage(expression_object.expression_matrix.transpose())
         try:
+            fig = plt.figure(figsize=(25,10))
             dn = dendrogram(tree)
-            plt.show()
+            if config.DEBUG:
+                plt.show()
+            plt.savefig(join(BasePaths.Images, "dendrogram.png"))
         except:
-            try:
-                plt.savefig(join(BasePaths.Images, "dendrogram.png"))
-            except:
-                print("Did not manage to create or save dendrogram")
+            print('Did not manage to create dendrogram')
         expression_object.tree = tree
         expression_object.name = self.out_file_name(expression_object.name)
         return expression_object
