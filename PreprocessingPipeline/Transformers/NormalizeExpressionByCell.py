097ba1b3c30865f85a4aed53aed453b7e336bc20
iriskalka <kalka.iris@gmail.com>
2018-05-02 15:06:32 +0300

    Add saving of raw data matrix

--- PreprocessingPipeline/Transformers/NormalizeExpressionByCell.py
+++ PreprocessingPipeline/Transformers/NormalizeExpressionByCell.py
@@ -9,5 +9,6 @@ class NormalizeExpressionByCell(Transformer):
     def transform_aux(self, expression_object, *args, **kwargs):
         expression_object.expression_matrix /= expression_object.expression_matrix.sum(axis=0)
         expression_object.expression_matrix *= 10 ** 6
+        expression_object.normalized_raw_expression = expression_object.expression_matrix.copy()
         expression_object.name = self.out_file_name(expression_object.name)
         return expression_object
